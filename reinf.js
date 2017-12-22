9b72e03fa59f355a8e9be378c12334fe4b87280d
KYLE LIPTON <lipton.kyle@gmail.com>
2017-12-22 14:40:48 -0500

    reinforcement

---
+++ reinf.js
@@ -0,0 +1,15 @@
+var items = ["purchase domain", "install SSL certificate", "deploy site", "celebrate success"];
+console.log("Todo:");
+for(var i = 0; i < items.length; i++){
+  var item = items[i]
+  console.log((i+1) + ':' , item);
+}
+
+// concatenate the pieces you want!!!
+
+console.log("hello", "from", "the", "other", "side");
+console.log("hello from the other side");
+console.log("hello" + "from");
+var z = 7;
+var output = "The number (" + z + ") is great.";
+console.log(output);
